4a05df31e0f7d204dc60660b9cba50c57ddd0d67
gaadhapnath <gaadha5pn@gmail.com>
2020-03-26 20:29:35 +0530

    template

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/gaadha_pnath/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         This is Gaadha
         </a>
       </header>
     </div>
